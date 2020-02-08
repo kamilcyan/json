26a54e8fb0ba963501e7a4279bc118ad262d2a4d
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-08 22:00:11 +0100

    trening

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -30,11 +30,18 @@ namespace ConsoleApp1
             product3.Price = 1.99f;
 
             products.Add(product);
-            products.Add(product1);
-            products.Add(product2);
-            products.Add(product3);
+            string output = JsonConvert.SerializeObject(product);
 
-            string output = JsonConvert.SerializeObject(products);
+            products.Add(product1);
+            string output1 = JsonConvert.SerializeObject(product1);
+
+            products.Add(product2);
+            string output2 = JsonConvert.SerializeObject(product2);
+
+            products.Add(product3);
+            string output3 = JsonConvert.SerializeObject(product3);
+
+
             //{
             //  "Name": "Apple",
             //  "ExpiryDate": "2008-12-28T00:00:00",
@@ -62,9 +69,12 @@ namespace ConsoleApp1
             Console.WriteLine(product.Name);
             Console.WriteLine(product.Price);
 
-            List<Product> deserializedProduct = JsonConvert.DeserializeObject<List<Product>>(output);
+            //List<Product> deserializedProduct = JsonConvert.DeserializeObject<List<Product>>(output);
 
             Console.WriteLine(output);
+            Console.WriteLine(output1);
+            Console.WriteLine(output2);
+            Console.WriteLine(output3);
 
             Console.ReadLine();
         }
